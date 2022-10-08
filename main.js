457d4f0878dfa85718262004774ace1dcb238b17
smily000 <91569111+smily000@users.noreply.github.com>
2022-10-08 12:59:22 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let timer
 let deleteFirstPhotoDelay
 
-
+///Daniel Reyes, Dogs, JavaScript & An API 🐶 Fetch, Promises & Async Await and your Danielreyes@lewisu.edu
+//Brad Schiff who created the application/
 
 async function start(){
     try{
